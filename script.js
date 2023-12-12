e46c818b81232ce05dd42e8c98b7c905765c719b
Ashraf Ayman <140410745+ashrafayman219@users.noreply.github.com>
2023-12-12 16:14:55 +0200

    script.js

--- script.js
+++ script.js
@@ -113,6 +113,18 @@ var polylineGraphic;
 var gra;
 var N = 6;
 
+
+// updated starting of declaration variables
+var array = [];
+var currentFeat = {
+  graphic: {},
+  obj: {},
+};
+
+var sidePoint = [];
+var errorMsg = document.getElementById("errMsg");
+// updated ending of declaration variables
+
 function loadModule(moduleName) {
   return new Promise((resolve, reject) => {
     require([moduleName], (module) => {
@@ -204,8 +216,24 @@ async function initializeMap() {
           { duration: 4000 }
         );
       });
+
+      // updated starting calling of draw
+      //draw graphics
+      drawGraphics()
+        .then(([view, displayMap]) => {
+          console.log("Array Returned From Require Scope", array);
+          // You can work with the view object here
+        })
+        .catch((error) => {
+          // Handle any errors here
+        });
+      // updated ending calling of draw
+      
+      
     }
-    return [view, displayMap, gL]; // You can return the view object
+          // updated start of return array
+    return [view, displayMap, gL, array]; // You can return the view object
+          // updated end of return array
   } catch (error) {
     console.error("Error initializing map:", error);
     throw error; // Rethrow the error to handle it further, if needed
@@ -509,9 +537,217 @@ async function getGraphics(arrayOfDisplayedGraphics) {
   }
 }
 
+// updated starting of draw
+async function drawGraphics() {
+  try {
+    const [Sketch, FeatureForm] = await Promise.all([
+      loadModule("esri/widgets/Sketch"),
+      loadModule("esri/widgets/FeatureForm"),
+    ]);
 
+    var form;
+    var modal = document.getElementById("modal");
 
+    // document.getElementById("btnSave").onclick = function () {
+    //     // Fires feature form's submit event.
+    //     form.submit();
+    //     modal.classList.add("hidden");
+    // };
 
+    // document.getElementById("btnCancel").onclick = function () {
+    //     modal.classList.add("hidden");
+    //     // console.log(res)
+    //     if (currentFeat.graphic){
+    //         graphicsLayer.remove(currentFeat.graphic);
+    //         currentFeat.graphic = {};
+    //         currentFeat.obj = {};
+    //         array.pop();
+    //         document.getElementById("projectName").value = "";
+    //         errorMsg.innerHTML = "";
+    //     }
+    // };
+
+    // var showModal = function() {
+    //     // document.getElementById("btnSave").disabled = true;
+    //     // // show the modal -
+    //     modal.classList.remove("hidden");
+    // };
+
+    // form = new FeatureForm({
+    //     container: "form",
+    //     groupDisplay: "sequential", // only display one group at a time
+    //     feature: graphicsLayer, // Pass in feature
+    // });
+
+    // form.on("submit", function() {
+    //     currentFeat.obj.LocationName = document.getElementById("projectName").value;
+    //     document.getElementById("projectName").value = "";
+    //     errorMsg.innerHTML = "";
+    // });
+
+    var sketch = new Sketch({
+      view: view,
+      layer: graphicsLayer,
+      availableCreateTools: ["polyline", "polygon", "point"],
+      // graphic will be selected as soon as it is created
+      creationMode: "update",
+      updateOnGraphicClick: true,
+      visibleElements: {
+        createTools: {
+          rectangle: false,
+          circle: false,
+        },
+        selectionTools: {
+          "lasso-selection": false,
+          "rectangle-selection": false,
+        },
+        settingsMenu: false,
+        undoRedoMenu: false,
+      },
+    });
+    view.ui.add(sketch, { position: "top-right", index: 0 });
+
+    var sketchVM = sketch.viewModel;
+    sketchVM.updateOnGraphicClick = false;
+    sketchVM.defaultUpdateOptions.enableRotation = false;
+    sketchVM.defaultUpdateOptions.toggleToolOnClick = false;
+    sketchVM.defaultUpdateOptions.tool = null;
+
+    // White fill color with 50% transparency
+    var fillColor = [255, 255, 255, 0.5];
+
+    // Red stroke, 1px wide
+    var stroke = {
+      color: [255, 0, 0],
+      width: 1,
+    };
+
+    // Override all default symbol colors and sizes
+    var pointSymbol = sketch.viewModel.pointSymbol;
+    pointSymbol.color = fillColor;
+    pointSymbol.outline = stroke;
+    pointSymbol.size = 8;
+
+    var polylineSymbol = sketch.viewModel.polylineSymbol;
+    polylineSymbol.color = stroke.color;
+    polylineSymbol.width = stroke.width;
+
+    var polygonSymbol = sketch.viewModel.polygonSymbol;
+    polygonSymbol.color = fillColor;
+    polygonSymbol.outline = stroke;
+
+    sketch.on("create", function (e) {
+      currentFeat.graphic = e.graphic;
+      var geometry = e.graphic.geometry;
+
+      if (e.state === "complete") {
+        if (geometry.type === "polygon") {
+          PolygonArray = [];
+          for (x in geometry.rings) {
+            var data = geometry.rings[x];
+            for (z in data) {
+              PolygonArray.push({
+                Latitude: data[z][0],
+                Longitude: data[z][1],
+              });
+            }
+          }
+          currentFeat.obj = {
+            CurrentMethod: geometry.type,
+            LocationName: "",
+            LocationPoints: PolygonArray,
+            idPolygon: new Date().getTime().toString(),
+          };
+          array.push(currentFeat.obj);
+        } else if (geometry.type === "polyline") {
+          PolylineArray = [];
+          for (x in geometry.paths) {
+            var data = geometry.paths[x];
+            for (z in data) {
+              PolylineArray.push({
+                Latitude: data[z][0],
+                Longitude: data[z][1],
+              });
+            }
+          }
+          currentFeat.obj = {
+            CurrentMethod: geometry.type,
+            LocationName: "",
+            LocationPoints: PolylineArray,
+            idPolyline: new Date().getTime().toString(),
+          };
+
+          array.push(currentFeat.obj);
+        } else if (geometry.type === "point") {
+          currentFeat.obj = {
+            CurrentMethod: geometry.type,
+            LocationName: "",
+            LocationPoints: [{ Latitude: geometry.x, Longitude: geometry.y }],
+            id: new Date().getTime().toString(),
+          };
+
+          array.push(currentFeat.obj);
+        } else {
+          console.log("No graphics added");
+        }
+      }
+    });
+
+    await view.when();
+    return array; // You can return the view object
+  } catch (error) {
+    console.error("Error initializing map:", error);
+    throw error; // Rethrow the error to handle it further, if needed
+  }
+}
+
+// updated ending of draw
+
+// updated starting of zooming
+async function zoomCoordinates(gra, N) {
+  try {
+    for (let o = 0; o < gra.length; o++) {
+      if (gra.items[o].attributes.id === N) {
+        view.goTo(
+          {
+            target: gra.items[o],
+            // zoom: 6
+          },
+          {
+            duration: 4000,
+          },
+          {
+            speedFactor: 0.2,
+            easing: "in-expo",
+          }
+        );
+        console.log(gra.items[o]);
+      } else {
+        console.log("No graphics here");
+      }
+    }
+    await view.when();
+    return gra; // You can return the view object
+  } catch (error) {
+    console.error("Error initializing map:", error);
+    throw error; // Rethrow the error to handle it further, if needed
+  }
+}
+
+// calling
+function s() {
+  zoomCoordinates(gra, N)
+    .then((gra) => {
+      console.log("coordinates Returned From Require Scope", gra);
+      // You can work with the view object here
+    })
+    .catch((error) => {
+      // Handle any errors here
+    });
+  view.when();
+}
+
+// updated ending of zooming
 
 
 
